cf913c22a5bc447bed0d8e2ad3efdfac23a6bdf5
Shakthi Kannan <author@shakthimaan.com>
2016-08-23 21:50:20 +0530

    Added POST method

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -1,6 +1,6 @@
 (ns guestbook.routes.home
   (:require [guestbook.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
             [guestbook.db.core :as db]))
 
@@ -9,10 +9,15 @@
    "home.html"
    {:messages (db/get-messages)}))
 
+(defn save-message! [{:keys [params]}]
+  (db/save-message!
+   (assoc params :timestamp (java.util.Date.)))
+  (response/found "/"))
+
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
+  (POST "/message" request (save-message! request))
   (GET "/about" [] (about-page)))
-
