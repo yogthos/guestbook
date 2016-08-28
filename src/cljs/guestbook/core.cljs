ed5a3ea3d39aa13712ea4376c326309e9e506453
Shakthi Kannan <author@shakthimaan.com>
2016-08-28 20:51:41 +0530

    Added form with reagent

--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -2,7 +2,29 @@
   (:require [reagent.core :as reagent :refer [atom]]))
 
 (defn home []
-  [:h2 "Hello, Reagent"])
+  [:div.row
+   [:div.span12
+    [message-form]]])
+
+(defn message-form []
+  (let [fields (atom {})]
+    (fn []
+      [:div.content
+       [:div.form-group
+        [:p "Name:"
+         [:input.form-control
+          {:type :text
+           :name :name
+           :on-change #(swap! fields assoc :name (-> % .-target .-value))
+           :value (:name @fields)}]]
+        [:p "Message:"
+         [:textarea.form-control
+          {:rows 4
+           :cols 50
+           :name :message
+           :value (:message @fields)
+           :on-change #(swap! fields assoc :message (-> % .-target .-value))}]]
+        [:input.btn.btn-primary {:type :submit :value "comment"}]]])))
 
 (reagent/render
  [home]
